e8409612c5470a4c3bdd70a58a7d063dcb8333af
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2015-04-28 00:03:17 -0400

    Initial implementation.

---
+++ facebook.js
@@ -0,0 +1,119 @@
+/**
+ * Implements hook_install().
+ */
+function facebook_install() {
+  try {
+    if (drupalgap.settings.mode == 'phonegap') {
+      openFB.init({
+        appId: drupalgap.settings.facebook.app_id[device.platform],
+        tokenStore: window.localStorage
+      });
+    }
+  }
+  catch (error) { console.log('facebook_install - ' + error); }
+}
+
+/**
+ * Implements hook_deviceready().
+ */
+function facebook_deviceready() {
+  try {
+    openFB.getLoginStatus(function(loginStatus) {
+        // Anonymous.
+        if (loginStatus.status == 'unknown') {
+          system_connect(_drupalgap_deviceready_options());
+        }
+        // Authenticated.
+        else if (loginStatus.status == 'connected') {
+          facebook_connected(loginStatus.authResponse.token);
+        }
+    });
+    return false;
+  }
+  catch (error) { console.log('facebook_deviceready - ' + error); }
+}
+
+/**
+ * Implements hook_services_request_pre_postprocess_alter().
+ */
+function facebook_services_request_pre_postprocess_alter(options, result) {
+  try {
+    if (options.service == 'fboauth' && options.resource == 'connect') { 
+      Drupal.sessid = result.token;
+    }
+  }
+  catch (error) { console.log('facebook_services_postprocess - ' + error); }
+}
+
+/**
+ * Implements hook_form_alter().
+ */
+function facebook_form_alter(form, form_state, form_id) {
+  try {
+    if (form_id == 'user_login_form') {
+      form.buttons['facebook'] = {
+        title: 'Login with Facebook',
+        attributes: {
+          onclick: "facebook_onclick()"
+        }
+      };
+    }
+  }
+  catch (error) { console.log('facebook_form_alter - ' + error); }
+}
+
+/**
+ *
+ */
+function facebook_onclick() {
+  try {
+    openFB.login(
+      function(response) {
+        if (response.status === 'connected') {
+          facebook_connected(response.authResponse.token);
+        }
+        else { drupalgap_alert(response.error); }
+      },
+      { scope: drupalgap.settings.facebook.scope }
+    );
+  }
+  catch (error) { console.log('facebook_onclick - ' + error); }
+}
+
+/**
+ *
+ */
+function facebook_connected(token) {
+  try {
+    fboauth_connect(token, {
+        success: function(result) {
+          if (result.user) {
+            system_connect(_drupalgap_deviceready_options({ reloadPage: true }));
+          }
+        },
+        error: function(xhr, status, message) {
+          if (status == 406 && message[0].indexOf('Already logged in as')) {
+            system_connect(_drupalgap_deviceready_options());
+          }
+        }
+    });
+  }
+  catch (error) { console.log('facebook_connected - ' + error); }
+}
+
+/**
+ * services_fboauth connect implementation.
+ */
+function fboauth_connect(token, options) {
+  try {
+    options.method = 'POST';
+    options.path = 'fboauth/connect.json';
+    options.data = JSON.stringify({
+        access_token: token
+    });
+    options.service = 'fboauth';
+    options.resource = 'connect';
+    Drupal.services.call(options);
+  }
+  catch (error) { console.log('fboauth_connect - ' + error); }
+}
